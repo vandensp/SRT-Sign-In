5685a7a14b68fa6583b519901b23f9edb770cb7a
Samuel Paul VanDenburgh <45343398+vandensp@users.noreply.github.com>
2023-04-27 10:45:10 -0400

    updated signIn.py

--- signIn.py
+++ signIn.py
@@ -1,9 +1,12 @@
 import serial
 import datetime
 import serial.tools.list_ports
+import pandas as pd
 
+studentRecord = pd.read_csv("studentRecords.csv")
 visitHistory = dict({"studentID":[], "name":[],"major":[],"Sign-in Time":[],"Sign-out Time":[]})
-roomStatus = dict({"Studnets In":[]})
+roomStatus = dict({"Students In":[]})
+historyDF = pd.DataFrame()
 
 comlist = serial.tools.list_ports.comports() #finds barcode port and opens it
 connected = []
@@ -12,28 +15,31 @@ for element in comlist:
     if element.description == "barcode scanner":
         ser = serial.Serial(element.device)
     print("Barcode Scanner connected at:", ser.name)
-    
+
 def storeVisit(student): #Keeps a list of all recorded scans
-        visitHistory["studentID"].append(student.id)
-        visitHistory["name"].append(student.name)
-        visitHistory["major"].apppend(student.major)
-        visitHistory["Sign-in Time "].append(student.inTime)
-        visitHistory["Sign-out Time "].append(student.outTime)
+        visitHistory["studentID"].append(student["studentID"])
+        visitHistory["name"].append(student["name"])
+        visitHistory["major"].append(student["major"])
+        visitHistory["Sign-in Time"].append(student["Sign-in Time"])
+        visitHistory["Sign-out Time"].append(student["Sign-out Time"]
         
 def signingIn(student): #Keeps a list of all recorded scans
         #edit students scan status and time
-        student.inTime = datetime.datetime.now().time()
+        student["Sign-in Time"] = datetime.datetime.now().time()
+        student['signingIn'] = "False"
+        print("Student Signed In!")
         
 def signingOut(student): #Keeps a list of all recorded scans
         #edit students scan status and histoyr
-        student.outTime = datetime.datetime.now().time()
+        student["Sign-out Time"] = datetime.datetime.now().time()
         #record visit
-        storeVisit(student)
-        
+        student['signingIn'] = "True"
+        print("Student Signed Out :(")
+
 def getStudent(line):
     #find student with matching ID
-    for student in students:
-        if student.id == line:
+    for student in studentRecord:
+        if student["studentID"] == line:
             return student
         else:
             print("Student Not Found")
@@ -42,12 +48,14 @@ while True: #checks for new input from barcode and updates dataframe
     line = ser.readline()
     line = line.decode('utf-8',errors='ignore').rstrip('/r/n')
     line = int(line)
-    student = getStudent(line)
-    if student.signingIn: #If student is signing in
-        addSignIn(student)
-        student.signingIn = False
+    student = studentRecord.loc[studentRecord['studentID'] == line]
+    ser.flush()
+    print("\n\nName",student.name,"\n\n")
+    if str(student['signingIn']) == "True": #If student is signing in
+        signingIn(student)
     else: #If student is signing out
-        addSignOut(student)
-        student.signingIn = True
+        signingOut(student)
+        storeVisit(student)
     #If time is pass hours
+    #    for student in Students
         for student in Students
