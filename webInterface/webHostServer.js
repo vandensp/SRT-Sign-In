c20c49ee22f7dae324f27bdbc16b0563f072617c
dfirestorm <haudj@rose-hulman.edu>
2023-05-18 10:12:01 -0400

    added SRT page setup

--- webInterface/webHostServer.js
+++ webInterface/webHostServer.js
@@ -1,6 +1,6 @@
 var http = require("http");    //#A
 var fs = require('fs');
-var index = fs.readFileSync('signInClient.html')
+var index
 var port = 1444;
 
 // anonymous function - function which handles requests to server
@@ -10,6 +10,10 @@ http.createServer(function(request,response){    //#B
     var x;
     var headers = request.headers;
     var trailers = request.trailers;
+    var url = request.url;
+    var argurl = url.toString().split('?')[1]
+    var params = argurl.split('&');
+    var args = {};
     console.log('New incoming client request for ' + request.url);
     console.log('Request method: ' + request.method);
     console.log('Headers: ');
@@ -19,6 +23,33 @@ http.createServer(function(request,response){    //#B
     console.log('Trailers: ');
     for (x in trailers) {
       console.log('\t{' + x + ': \"' + trailers[x] + '\"}');
+    }
+    console.log('full url: ');
+    console.log(url.toString());
+    argurl = argurl.substring(argurl.indexOf('&') + 1);
+    console.log('arg url: ');
+    console.log(argurl);
+    console.log('Params: ');
+    // separate out parameters from url
+    for (x in params) {
+        var y = params[x].split('=');
+        if (y[0] === 'path') {
+            path = y[1].replace('%2F', '/');
+            console.log('\t{' + y[0] + ': \"' + path + '\"}');
+        }
+        else {
+            args[y[0]] = y[1];
+            console.log('\t{' + y[0] + ': \"' + args[y[0]] + '\"}');
+        }
+    }
+    if (argurl === 'client'){
+        index= fs.readFileSync('signInClient.html')
+    }
+    else if (argurl === 'srt'){
+        index = fs.readFileSync('signInSRT.html')
+    }
+    else{
+        index= fs.readFileSync('signInClient.html')
     }
     response.writeHead(200,    //#C
           {'Content-Type': 'text/html',    //#D
